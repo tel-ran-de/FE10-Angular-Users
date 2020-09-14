853048e86d40cfe4e7c58f8bd98bf8db2ef84dd8
Alex Loginov <alexander.loginov@takeaway.com>
2020-09-14 21:30:23 +0200

    Observables - pipes

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {User} from '../model/user';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +17,10 @@ export class UserService {
     return this.httpClient.get<User[]>('assets/users.json');
   }
 
-  getUserById(id: string): User {
-    return this.users.find(user => user.id === id);
+  getUserById(id: string): Observable<User> {
+    return this.getUsers().pipe(
+      map(users => users.find(user => user.id === id))
+    );
   }
 
   getFilteredUsers(filter: {key: string, value: any}[]): User[] {
