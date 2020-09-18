ca2c693466ac0e1239471c367ae2e23054fc7456
Alex Loginov <alexander.loginov@takeaway.com>
2020-09-18 20:33:52 +0200

    Observables - async pipe in html template

--- src/app/component/user-layout/user-layout.component.ts
+++ src/app/component/user-layout/user-layout.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {User} from '../../model/user';
 import {UserService} from '../../service/user.service';
 import {ActivatedRoute} from '@angular/router';
 import {flatMap} from 'rxjs/operators';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user-layout',
   templateUrl: './user-layout.component.html',
   styleUrls: ['./user-layout.component.css']
 })
-export class UserLayoutComponent implements OnInit {
+export class UserLayoutComponent implements OnInit, OnDestroy {
   user: User;
   asyncUser: Observable<User>;
+  userSubscription: Subscription;
 
   constructor(private route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit(): void {
-    this.route.params.pipe(
+    this.userSubscription = this.route.params.pipe(
       flatMap(params => this.userService.getUserById(params.id))
     ).subscribe(user => this.user = user);
     this.asyncUser = this.getUser();
+  }
+
+  ngOnDestroy(): void {
+   this.userSubscription.unsubscribe();
   }
 
   getUser(): Observable<User> {
